8acae571a82aea26e9fbed712391f1f6fc072f48
Matthew Tutak <mtutak91@gmail.com>
2017-03-30 16:17:41 -0500

    server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var bodyParser = require("body-parser");
 const passport = require('passport');
 const mongoose = require('mongoose');
 
-// var bluetooth = require('node-bluetooth');
+var bluetooth = require('node-bluetooth');
 
-// var bluetooth = require('./build/Release/node-bluetooth');
+var bluetooth = require('./build/Release/node-bluetooth');
 
 
 const Project = require('./models/Project');
@@ -46,123 +46,123 @@ app.use('/auth', authRoutes);
 const apiRoutes = require('./controllers/api');
 app.use('/api', apiRoutes);
 
-// var User = require("./models/user.js");
-// const jwt = require('jsonwebtoken');
-// const config = require('./app/config/config.json');
+var User = require("./models/user.js");
+const jwt = require('jsonwebtoken');
+const config = require('./app/config/config.json');
 
-// app.get('/bluetooth', function(req, res) {
-// 	console.log('im here');
+app.get('/bluetooth', function(req, res) {
+	console.log('im here');
 
-//     // create bluetooth device instance
-//     const device = new bluetooth.DeviceINQ();
+    // create bluetooth device instance
+    const device = new bluetooth.DeviceINQ();
 
-//     var listOfDevices = {devices:[]};
+    var listOfDevices = {devices:[]};
 
-//     device
-//     .on('finished', function(){setTimeout(function(){
-// 		    res.json(listOfDevices); 
-//     }, 3000)})
-//     .on('found', function found(address, name){
+    device
+    .on('finished', function(){setTimeout(function(){
+		    res.json(listOfDevices); 
+    }, 3000)})
+    .on('found', function found(address, name){
       
-//       console.log('Found: ' + address + ' with name ' + name);
+      console.log('Found: ' + address + ' with name ' + name);
       
-//       // find serial port channel
-//       device.findSerialPortChannel(address, function(channel){
-//         console.log('Found RFCOMM channel for serial port on %s: ', name, channel);
-// 		var foundDevice={};
-// 		foundDevice['name'] = name;
-// 		foundDevice['address'] = address;
-// 		foundDevice['channel'] = channel;
-// 		listOfDevices['devices'].push(foundDevice);
-// 		console.log(listOfDevices);
-// 		    // res.json({
-// 		    //   nameOfPhone: name
-// 		    // }); 
-//         // make bluetooth connect to remote device
-//       //   bluetooth.connect(address, channel, function(err, connection){
-//       //     if(err) return console.error(err);
-//       //     connection.write(new Buffer('Hello!', 'utf-8'), function(){
-//       //     	console.log('hey!');});
-// 		    // res.json({
-// 		    //   paired: true
-// 		    // });
-//       //     });
+      // find serial port channel
+      device.findSerialPortChannel(address, function(channel){
+        console.log('Found RFCOMM channel for serial port on %s: ', name, channel);
+		var foundDevice={};
+		foundDevice['name'] = name;
+		foundDevice['address'] = address;
+		foundDevice['channel'] = channel;
+		listOfDevices['devices'].push(foundDevice);
+		console.log(listOfDevices);
+		    // res.json({
+		    //   nameOfPhone: name
+		    // }); 
+        // make bluetooth connect to remote device
+      //   bluetooth.connect(address, channel, function(err, connection){
+      //     if(err) return console.error(err);
+      //     connection.write(new Buffer('Hello!', 'utf-8'), function(){
+      //     	console.log('hey!');});
+		    // res.json({
+		    //   paired: true
+		    // });
+      //     });
         
-//       	} );
+      	} );
       
-//     }).inquire();
-// });
+    }).inquire();
+});
 
 
-// app.post('/bluetooth/confirm', function(req, res) {
-// 	console.log('AYE IM BACK');
-// 	// console.log(req);
+app.post('/bluetooth/confirm', function(req, res) {
+	console.log('AYE IM BACK');
+	// console.log(req);
 
-// 	var sentDeviceInfo=req.body;
+	var sentDeviceInfo=req.body;
 
-//     // create bluetooth device instance
-//     const device = new bluetooth.DeviceINQ();
+    // create bluetooth device instance
+    const device = new bluetooth.DeviceINQ();
 
-//     // var listOfDevices = {devices:[]};
+    // var listOfDevices = {devices:[]};
 
-//     device
-//     .on('finished', console.log.bind(console, 'finished'))
-//     .on('found', function found(address, name){
+    device
+    .on('finished', console.log.bind(console, 'finished'))
+    .on('found', function found(address, name){
       
-//       console.log('Found: ' + address + ' with name ' + name);
-//       if(address==sentDeviceInfo.deviceAddress){
-// 	      // find serial port channel
-// 	      device.findSerialPortChannel(address, function(channel){
-// 	        console.log('Found RFCOMM channel for serial port on %s: ', name, channel);
-// 			// var foundDevice=address;
+      console.log('Found: ' + address + ' with name ' + name);
+      if(address==sentDeviceInfo.deviceAddress){
+	      // find serial port channel
+	      device.findSerialPortChannel(address, function(channel){
+	        console.log('Found RFCOMM channel for serial port on %s: ', name, channel);
+			// var foundDevice=address;
 
-// 			    // res.json({
-// 			    //   nameOfPhone: name
-// 			    // }); 
-// 	        // make bluetooth connect to remote device
-// 	        bluetooth.connect(address, channel, function(err, connection){
-// 	          if(err){res.send(err)};
-// 	          connection.write(new Buffer('Hello!', 'utf-8'), function(){
-// 	          	// console.log('hey!');
+			    // res.json({
+			    //   nameOfPhone: name
+			    // }); 
+	        // make bluetooth connect to remote device
+	        bluetooth.connect(address, channel, function(err, connection){
+	          if(err){res.send(err)};
+	          connection.write(new Buffer('Hello!', 'utf-8'), function(){
+	          	// console.log('hey!');
 
-// 		    	User.findOneAndUpdate({_id: sentDeviceInfo.loggedInUser}, { $pull: { "pendingConnections": sentDeviceInfo.friendToAdd }, $push: { "connections": sentDeviceInfo.friendToAdd } }, { new: true }, function(err, newdoc) {
-// 				        // Send any errors to the browser
-// 				        if (err) {
-// 				          res.send(err);
-// 				        }
-// 				        // Or send the newdoc to the browser
-// 				        else {
+		    	User.findOneAndUpdate({_id: sentDeviceInfo.loggedInUser}, { $pull: { "pendingConnections": sentDeviceInfo.friendToAdd }, $push: { "connections": sentDeviceInfo.friendToAdd } }, { new: true }, function(err, newdoc) {
+				        // Send any errors to the browser
+				        if (err) {
+				          res.send(err);
+				        }
+				        // Or send the newdoc to the browser
+				        else {
 
-// 					       	User.findOneAndUpdate({_id: sentDeviceInfo.friendToAdd}, { $pull: { "pendingConnections": sentDeviceInfo.loggedInUser }, $push: { "connections": sentDeviceInfo.loggedInUser } }, { new: true }, function(err, newdoc) {
-// 					        	// Send any errors to the browser
-// 						        if (err) {
-// 						          res.send(err);
-// 						        }
-// 						        // Or send the newdoc to the browser
-// 						        else {
-// 				    res.json({
-// 				      friendAdded: true 
-// 				    }); 	 
+					       	User.findOneAndUpdate({_id: sentDeviceInfo.friendToAdd}, { $pull: { "pendingConnections": sentDeviceInfo.loggedInUser }, $push: { "connections": sentDeviceInfo.loggedInUser } }, { new: true }, function(err, newdoc) {
+					        	// Send any errors to the browser
+						        if (err) {
+						          res.send(err);
+						        }
+						        // Or send the newdoc to the browser
+						        else {
+				    res.json({
+				      friendAdded: true 
+				    }); 	 
 
-// 						        }
-// 					    	})
+						        }
+					    	})
 
-// 				        }
-// 				});
+				        }
+				});
 
 
-// 			    // res.json({
-// 			    //   paired: true
-// 			    // });
-// 	          });
-// 	          });
+			    // res.json({
+			    //   paired: true
+			    // });
+	          });
+	          });
 	        
-// 	      	} );
+	      	} );
       	
-//       }
+      }
       
-//     }).inquire();
-// });
+    }).inquire();
+});
 
 app.post('/projects/new', function(req, res){
       console.log('hit project new route!');
